052cfaf58e9c2bded59e66c582ab331dc407392e
opmvpc <thibsix@outlook.be>
2023-10-01 16:43:07 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,33 @@
+import * as bootstrap from 'bootstrap';
+import './style.scss';
+import { data } from './data';
+import { nav } from './nav';
+
+const listePersonnes = () => {
+  let html = '';
+  for (let i = 0; i < data.length; i++) {
+    const personne = data[i];
+    let personneCard = `
+      <a class="card col-5 col-md-3" href="/personne/?id=${personne.id}">
+        <img src="${personne.avatar}" class="card-img-top" alt="avatar de ${personne.prenom} ${personne.nom}">
+        <div class="card-body">
+          <h5 class="card-title">${personne.prenom} ${personne.nom}</h5>
+        </div>
+      </a>
+    `;
+    html += personneCard;
+  }
+  return html;
+};
+
+document.querySelector('#app').innerHTML = `
+  <main>
+    ${nav}
+
+    <div class="container-fluid my-4">
+      <div class="d-flex gap-3 flex-wrap justify-content-center">
+        ${listePersonnes()}
+      </div>
+    </div>
+  </main>
+`;
